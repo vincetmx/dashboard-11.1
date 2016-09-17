463142f1186b5c43408f62e5466ca5cec6ed3218
suncihai <suncihai@gmail.com>
2016-09-16 23:00:20 -0400

    cihai upload his file

--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -2,29 +2,71 @@
 // WORK CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Work" section to display items and filter them
-app.controller('workCtrl',function($scope,$http){
+app.controller('workCtrl',function($scope,$http,passhelper){
 	$http.get("/api/work")
 	.then(function(response){
         $scope.works = response.data.works;
 	});
+    
+    $scope.myVar1 = true;
+    $scope.myVar2 = false;
+
+    $scope.toggle = function(value){
+       $scope.myVar1 = !value;
+       $scope.myVar2 = value;
+       console.log($scope.myVar1);
+       console.log($scope.myVar2);
+    }
+
+    $scope.orderByMe=function(name){
+    	//myorder is defined in DOM, name is the rule that wants to order by
+    	$scope.myorder=name;
+    }
+    var tmp=11;
+
+	$scope.submit=function(){
+		var obj={};
+        obj.id=tmp++;
+        obj.title=$scope.nameAdd;
+        obj.author=$scope.authorAdd;
+        obj.like=$scope.likeAdd;
+        obj.comment=$scope.commentAdd;
+        console.log(obj);
+        $scope.works.push(obj);
+        $scope.nameAdd=null;
+        $scope.authorAdd=null;
+        $scope.likeAdd=null;
+        $scope.commentAdd=null;
+        console.log($scope.works[$scope.works.length-1].id);
+    };
+    
+    $scope.deleteconfirm=function(work){
+        $scope.removeIndex = $scope.works.indexOf(work);
+    }
+
+    $scope.delete=function(){
+        $scope.works.splice($scope.removeIndex,1);
+    }
+
+    $scope.editconfirm=function(work){
+       $scope.editIndex = $scope.works.indexOf(work);
+       console.log($scope.works[$scope.editIndex].title);
+       console.log($scope.editTitle);
+       console.log("testtest");
+    };
+
+    $scope.editor=function(){
+        $scope.works[$scope.editIndex].title=passhelper.title;
+        $scope.works[$scope.editIndex].author=passhelper.author;
+        $scope.works[$scope.editIndex].like=passhelper.like;
+        $scope.works[$scope.editIndex].comment=passhelper.comment;
+    }
+}).factory('passhelper',function(){
+    return {
+       title:"",
+       author:"",
+       like:"",
+       comment:""
+    };
 });
-// });controller('addCtrl',function($scope){
-// 	$scope.submitForm=function(){
-// 		return function(){
-//            var idcount=11;
-//            return function(){
-//              var newtile ={};
-//              newtile.id=icount;
-// 		     newtile.title=$scope.tile_title;
-// 			 newtile.author=$scope.tile_author;
-// 			 newtile.like=$scope.tile_like;
-// 			 newtile.comment=$scope.tile_comment;
 
-//            }
-// 		}()
-
-
-// 	}
-// }).factory('addworks',function(){
-// 	return {};
-// });
--- app/scripts/directives/dialog.js
+++ app/scripts/directives/dialog.js
@@ -2,27 +2,45 @@
 // DIALOG DIRECTIVE
 // Description: Define 2 directives in the app: addDialog and editDialog. 
 // First one is for the "Add" button and the second one is for the "Edit" button
-app.directive("addDirective",function(){
+app.directive('addDialog',function(){
 	return {
-		restrict:'A',
+		restrict:'EA',
 		replace:true,
-		template:'\
-    <div>\
-    <button class="btn btn-danger" data-toggle="modal" data-target="#add_modal">Add Item</button>\
-		<div id="add_modal" role="dialog" tabindex="-1" class="modal fade" aria-labelledby="titlelabel" aria-hidden="true">\
-            <div class="modal-dialog">\
-                <div class="modal-content" id="window_content">\
-                   <h1 class="modal-title" id="titlelabel">Add Item</h1>\
-                   <input type="text" ng-model="tile_title" class="form-control" placeholder="Title">\
-                   <input type="text" ng-model="tile_author" class="form-control" placeholder="Author">\
-                   <input type="text" ng-model="tile_like" class="form-control" placeholder="Like">\
-                   <input type="text" ng-model="tile_comment" class="form-control" placeholder="Comment">\
-                    <button type="button" class="btn btn-success" ng-click="submitForm()" ng-disabled="!tile_title||!tile_author||!tile_like||!tile_comment">Submit</button>\
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>\
-                </div>\
-            </div>\
-        </div>\
-        </div>\
-        '
-	}
+		templateUrl:'../../templates/addDialog.html'
+	};
+});
+app.directive('editDialog',function(){
+	return {
+		restrict:'EA',
+		replace:true,
+		scope:{
+			editTitle:'@editName',
+			editAuthor:'@editAuthor',
+			editLike:'@editLike',
+			editComment:'@editComment',
+			editor:"&"
+		},
+		controller:function($scope,passhelper){
+           $scope.$watch('editTitle',function(newValue,oldValue){
+               passhelper.title=$scope.editTitle;
+           });
+           $scope.$watch('editAuthor',function(newValue,oldValue){
+               passhelper.author=$scope.editAuthor;
+           });
+           $scope.$watch('editLike',function(newValue,oldValue){
+               passhelper.like=$scope.editLike;
+           });
+           $scope.$watch('editComment',function(newValue,oldValue){
+               passhelper.comment=$scope.editComment;
+           });
+		},
+		templateUrl:'../../templates/editDialog.html'
+	};
+});
+app.directive('deleteDialog',function(){
+	return {
+		restrict:'EA',
+		replace:true,
+		templateUrl:'../../templates/confirmBox.html'
+	};
 });
